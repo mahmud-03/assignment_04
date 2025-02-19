87f1262229125b5972f2df1e171c980a3aedb0b1
mahmud-o3 <almahmudislamnaime@gmail.com>
2025-02-19 08:22:59 +0800

    email check

---
+++ email_check.js
@@ -0,0 +1,37 @@
+function validEmail(email) {
+  if (typeof email !== "string") {
+    return "invalid";
+  }
+  let spacialChartes = [".", "@", "_", "-", "+"];
+  let firstLatter = email.charAt(0);
+  for (let i = 0; i < spacialChartes.length; i++) {
+    if (firstLatter === spacialChartes[i]) {
+      return "false";
+    }
+  }
+  let firstSpace = false;
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === " ") {
+      firstSpace = true;
+      break;
+    }
+  }
+  if (firstSpace === true) {
+    return false;
+  }
+  let atderSymbol = false;
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === "@") {
+      atderSymbol === true;
+      break;
+    }
+  }
+  if (atderSymbol === false) {
+    return false;
+  }
+  let lastFourLatter = "";
+  if (email.length >= 4) {
+  }
+}
+
+console.log(email(islam.@gmail.com))
